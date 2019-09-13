11f8c228d4828294015df27cf155f17f9cc5822a
André "MG" Wisén <andre.wisen@gmail.com>
2019-09-13 10:04:53 +0200

    Add first header

---
+++ app/api.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  * Makes a remote HTTP request and returns the response.
+  *
+  * @author    André Wisén & Arvid Viderberg
+  *
+  * Credit: https://girders.org/2018/07/php-guide-to-http-api-requests.html
+  * More info: https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
+*/
+
+// Debug
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+
+?>
